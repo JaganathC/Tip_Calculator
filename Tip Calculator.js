e02be5431f9d1b517e01d6f3934718ab7df66d40
CHOWDAM JAGANATH <123239098+JaganathC@users.noreply.github.com>
2023-06-10 11:11:11 +0530

    Add files via upload

---
+++ Tip Calculator.js
@@ -0,0 +1,30 @@
+let billAmountInput = document.getElementById("billAmount");
+let percentageTipInput = document.getElementById("percentageTip");
+let tipAmountInput = document.getElementById("tipAmount");
+let totalInput = document.getElementById("totalAmount");
+
+let errorMessageElement = document.getElementById("errorMessage");
+
+function calculate() {
+    let billAmountInputValue = billAmountInput.value;
+    let percentageTipInputValue = percentageTipInput.value;
+    let errorMessage = "Please Enter a Valid Input";
+
+
+    if (billAmountInputValue === "") {
+        errorMessageElement.textContent = errorMessage;
+    } else if (percentageTipInputValue === "") { //Here, if the percentage tip input value is empty, error message will be displayed
+        errorMessageElement.textContent = errorMessage;
+    } else {
+        errorMessageElement.textContent = ""; //Here, if the both input values are not empty, error message will be removed
+        let billAmount = parseInt(billAmountInputValue); //getting bill amount and converting to integer
+        let percentageTip = parseInt(percentageTipInputValue); //getting tip percentage and converting to integer
+
+        let calculatedTip = (percentageTip / 100) * billAmount; //calculating tip
+        let calculatedTotal = billAmount + calculatedTip; //calculating total
+
+
+        tipAmountInput.value = calculatedTip; //assigning CalculatedTip to the input
+        totalInput.value = calculatedTotal; //assigning total bill amount to the input
+    }
+}
